df4800565959a8908be00077dd4c9e0b13b8671c
kongwonkeun <kongwonkeun@gmail.com>
2019-11-15 17:15:42 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿//
+//
+//
+
+using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+
+namespace KRollerClientCs
+{
+    class Program
+    {
+        //const string PIPE_FOO = "\\\\.\\pipe\foo";
+        const string PIPE_FOO = "foo";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("c: pipe client");
+
+            using (var client = new NamedPipeClientStream(PIPE_FOO))
+            {
+                Console.WriteLine("c: try to connect");
+                client.Connect();
+                using (var reader = new StreamReader(client))
+                {
+                    char[] s = new char[1024];
+                    Console.WriteLine("c: read");
+                    while (true)
+                    {
+                        var n = reader.Read(s, 0, s.Length - 1);
+                        if (n != 0)
+                        {
+                            Console.WriteLine(s, 0, n);
+                        }
+
+                        if (Console.KeyAvailable == true)
+                        {
+                            var c = Console.ReadKey(true);
+                            if (c.KeyChar == 'q')
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("c: broken pipe, bye bye");
+        }
+    }
+}
+
+//
+// eof
+//
